8fe84c283db2ce465e485aa34ebddeeb28dee22c
Josh Barrow <josh@jukaela.com>
2012-05-01 17:27:46 -0500

    Remove javascrip cruft

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require bootstrap-tooltip.js
-//= require bootstrap-popover.js
 //= require_tree .
-
-$("#example").popover(placement: 'right')
