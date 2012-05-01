64bc9a26d491cf7409e45656a6851982161dd2a0
Josh Barrow <josh@jukaela.com>
2012-05-01 14:55:43 -0500

    Major changes to timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,4 +77,6 @@ class UsersController < ApplicationController
   def admin_user
     redirect_to(root_path) unless current_user.admin
   end
+
+    
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,16 @@ module UsersHelper
     :class => 'gravatar',
     :gravatar => options)
   end
+
+  def quoted_user(username)
+    url = '"%s" ' % username
+  end
+
+  def unique_modal(id, type)
+    id.to_s + type
+  end
+  
+  def link_to_unique_modal(string)
+    return_string = '#%s' % string
+  end
 end
