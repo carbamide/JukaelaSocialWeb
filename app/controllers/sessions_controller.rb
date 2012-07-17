439a7ee1efa15d4ae55a57dee1573bbea0f5cc0a
Josh Barrow <josh@jukaela.com>
2012-07-17 14:55:45 -0500

    Huge amount of work.  Too much to list.  Hopefilly this commit fixes the Xcode git stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,38 +2,42 @@ class SessionsController < ApplicationController
   def new
     @title = "Sign in"
   end
-
+  
   def create
     session[:staysignedin] = (params[:session][:stay_signed_in] == "1") ? true : false
     
     user = User.authenticate(params[:session][:email], params[:session][:password])
     
-    apn = user.apns.build(:device_token => params[:session][:apns])
-        
-    apn.save
-
-    user.apns.each do |a|
-      Urbanairship.register_device a.device_token
+    if params[:session][:apns]
+      apn = user.apns.build(:device_token => params[:session][:apns])
+      
+      apn.save
+    end
+    
+    if user
+      user.apns.each do |a|
+        Urbanairship.register_device a.device_token
+      end
     end
     
     respond_to do |format|
-          format.html {
-            if user.nil?
-              flash.now[:error] = "Invalid email/password combination."
-              @title = "Sign in"
-              render 'new'
-            else
-              sign_in user
-              redirect_back_or root_path
-            end
-          }
-          format.json  { 
-            sign_in user
-            
-            render :json => user.to_json
-          }
+      format.html {
+        if user.nil?
+          flash.now[:error] = "Invalid email/password combination."
+          @title = "Sign in"
+          render 'new'
+          else
+          sign_in user
+          redirect_back_or root_path
+        end
+      }
+      format.json  { 
+        sign_in user
+        
+        render :json => user.to_json
+      }
     end
-
+    
   end
   
   def destroy
