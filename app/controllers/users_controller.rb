073380bef909b1507d407a29a8ebf7978e31cf1a
Josh Barrow <josh@jukaela.com>
2012-07-19 10:50:40 -0500

    Fix some stuff with users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,115 +1,117 @@
 class UsersController < ApplicationController
-  
-  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
-  before_filter :correct_user, :only => [:edit, :update]
-  before_filter :admin_user,   :only => :destroy
-
-  def new
-    @user = User.new
-    @title = "Sign up"
-  end
-
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      UserMailer.welcome_email(@user).deliver
+    
+    before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+    before_filter :correct_user, :only => [:edit, :update]
+    before_filter :admin_user,   :only => :destroy
+    
+    def new
+        @user = User.new
+        @title = "Sign up"
+    end
+    
+    def create
+        @user = User.new(params[:user])
+        if @user.save
+            UserMailer.welcome_email(@user).deliver
+            
+            sign_in @user
+            flash[:success] = "Welcome"
+            redirect_to @user
+            else
+            @title = "Sign up"
+            render 'new'
+        end
+    end
+    
+    def edit
+        @title = "Edit user"
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        #raise params.inspect
+        if @user.update_attributes(params[:user])
+            flash[:success] = "Profile updated."
+            
+            redirect_to @user
+            else
+            @title = "Edit user"
+            render 'edit'
+        end
+    end
+    
+    def index
+        @title = "All users"
+        @users = User.paginate(:page => params[:page])
         
-      sign_in @user
-      flash[:success] = "Welcome"
-      redirect_to @user
-    else
-      @title = "Sign up"
-      render 'new'
+        respond_to do |format|
+            format.html # index.html.erb
+            format.json  { render :json => User.all }
+        end
     end
-  end
-
-  def edit
-    @title = "Edit user"
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated."
-      redirect_to @user
-    else
-      @title = "Edit user"
-      render 'edit'
+    
+    def show
+        @user = User.find(params[:id])
+        @microposts = @user.microposts.paginate(:page => params[:page])
+        @mentions = @user.mentions.paginate(:page => params[:page])
+        @title = @user.name
+        
+        respond_to do |format|
+            format.html
+            format.json  { render :json => @user }
+        end
     end
-  end
-
-  def index
-    @title = "All users"
-    @users = User.paginate(:page => params[:page])
     
-    respond_to do |format|
-          format.html # index.html.erb
-          format.json  { render :json => User.all }
+    def show_microposts_for_user
+        @user = User.find(params[:id])
+        @microposts = @user.microposts.all
+        
+        respond_to do |format|
+            format.json { render :json => @microposts }
+        end
     end
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @microposts = @user.microposts.paginate(:page => params[:page])
-    @mentions = @user.mentions.paginate(:page => params[:page])
-    @title = @user.name
     
-    respond_to do |format|
-      format.html
-      format.json  { render :json => @user }
+    def destroy
+        User.find(params[:id]).destroy
+        flash[:success] = "User destroyed."
+        redirect_to users_path
     end
-  end
-
-  def show_microposts_for_user
-  	@user = User.find(params[:id])
-  	@microposts = @user.microposts.all
-  	
-  	respond_to do |format|
-  		format.json { render :json => @microposts }
-  	end
-  end
-  
-  def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_path
-  end
-
-  def following
-    @title = "Following"
-    @user = User.find(params[:id])
-    @users = @user.following.paginate(:page => params[:page])
     
-    raise @users.inspect
-
-    respond_to do |format|
-      format.html { render 'show_follow' }
-      format.json { render :json => @user.following.all }
+    def following
+        @title = "Following"
+        @user = User.find(params[:id])
+        @users = @user.following.paginate(:page => params[:page])
+        
+        raise @users.inspect
+        
+        respond_to do |format|
+            format.html { render 'show_follow' }
+            format.json { render :json => @user.following.all }
+        end
     end
-  end
-
-  def followers
-    @title = "Followers"
-    @user = User.find(params[:id])
-    @users = @user.followers.paginate(:page => params[:page])
     
-    raise @user.inspect
+    def followers
+        @title = "Followers"
+        @user = User.find(params[:id])
+        @users = @user.followers.paginate(:page => params[:page])
+        
+        raise @user.inspect
+        
+        respond_to do |format|
+            format.html { render 'show_follow' }
+            format.json { render :json => @user.followers.all }
+        end 
+    end
     
-    respond_to do |format|
-      format.html { render 'show_follow' }
-      format.json { render :json => @user.followers.all }
-    end 
-  end
-
-  private
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
-  end
-
-  def admin_user
-    redirect_to(root_path) unless current_user.admin
-  end
-
+    private
+    def correct_user
+        @user = User.find(params[:id])
+        redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+        redirect_to(root_path) unless current_user.admin
+    end
+    
     
 end
