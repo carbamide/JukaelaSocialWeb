ae73bee344ee9a02b55b1093778cd82698a3b373
Josh Barrow <josh@jukaela.com>
2012-12-12 13:15:04 -0600

    Adding support for direct messages

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -4,20 +4,24 @@ class DirectMessagesController < ApplicationController
     
     def index
         respond_to do |format|
-            format.html # index.html.erb
             format.json  { render :json => current_user.direct_messages.all }
         end
     end
     
     def create
-        @direct_message = current_user.direct_messages.build(params[:direct_message])
-        
-        if @direct_message.save
-            flash[:success] = "direct_message created!"
-            redirect_to root_path
-            else
-            @feed_items = []
-            render 'pages/home'
+        respond_to do |format|
+            format.json  {
+                user_message_is_going_to = User.find_by_username(params[:username]);
+                
+                @direct_message = user_message_is_going_to.direct_messages.build(:content => params[:direct_message][:content],
+                                                                                 :from_name => current_user.name,
+                                                                                 :from_user_id => current_user.id,
+                                                                                 :from_username => current_user.username,
+                                                                                 :from_email => current_user.email,
+                                                                                 :user_id => user_message_is_going_to.id)
+                
+                @direct_message.save
+            }
         end
     end
     
