77945bd5dd3b650e6582aad77a4297c9a17231b8
Josh Barrow <josh@jukaela.com>
2013-01-04 14:32:28 -0600

    I clearly don't know what I'm doing. part three

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,71 +1,68 @@
 class SessionsController < ApplicationController
-    def new
-        @title = "Sign in"
-    end
+  def new
+    @title = "Sign in"
+  end
     
-    def create
-        session[:staysignedin] = (params[:session][:stay_signed_in] == "1") ? true : false
+  def create
+    session[:staysignedin] = (params[:session][:stay_signed_in] == "1") ? true : false
         
-        user = User.authenticate(params[:session][:email], params[:session][:password])
+    user = User.authenticate(params[:session][:email], params[:session][:password])
         
-        if params[:session][:apns]
-            save_device_token = true
+    if params[:session][:apns]
+      save_device_token = true
             
-            if params[:session][:apns].length == 64
-                apn = user.apns.build(:device_token => params[:session][:apns])
-                
-                user.apns.each do |a|
-                    if a.device_token.eql?(params[:session][:apns])
-                        save_device_token = false
-                    end
-                end
-            else
-                apn = user.apids.build(:device_token => params[:session][:apns])
-                
-                user.apids.each do |a|
-                    if a.device_token.eql?(params[:session][:apns])
-                        save_device_token = false
-                    end
-                end
-            end
-            
-            if save_device_token == true
-                apn.save
-            end
+      if params[:session][:apns].length == 64
+        apn = user.apns.build(:device_token => params[:session][:apns])
+      else
+        apn = user.apids.build(:device_token => params[:session][:apns])
+      end
+         
+      user.apns.each do |a|
+        if a.device_token.eql?(params[:session][:apns])
+          save_device_token = false
         end
-        
-        if user
-            user.apns.each do |a|
-                Urbanairship.register_device a.device_token
-            end
-            
-            user.apids.each do |ap|
-                Urbanairship.register_device(ap.device_token, :provider => :android)
-            end
-        end
-        
-        respond_to do |format|
-            format.html {
-                if user.nil?
-                    flash.now[:error] = "Invalid email/password combination."
-                    @title = "Sign in"
-                    render 'new'
-                else
-                    sign_in user
-                    redirect_back_or root_path
-                end
-            }
-            format.json  {
-                sign_in user
-                
-                render :json => user.to_json
-            }
-        end
-        
+      end
+         
+      if save_device_token == true
+        puts "Saving the device token!"
+        apn.save
+      else 
+        puts "Not saving the device token!"
+      end
     end
+        
+    if user
+      user.apns.each do |a|
+        Urbanairship.register_device a.device_token
+      end
+            
+      user.apids.each do |ap|
+        Urbanairship.register_device(ap.device_token, :provider => :android)
+      end
+    end
+        
+    respond_to do |format|
+      format.html {
+        if user.nil?
+          flash.now[:error] = "Invalid email/password combination."
+          @title = "Sign in"
+          render 'new'
+        else
+          sign_in user
+          redirect_back_or root_path
+        end
+      }
+      format.json  {
+        sign_in user
+                
+        render :json => user.to_json
+      }
+    end
+        
+  end
     
-    def destroy
-        sign_out
-        redirect_to root_path
-    end
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
 end
